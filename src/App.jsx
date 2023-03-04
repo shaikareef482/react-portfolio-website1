7d345d664977586e552ce419a2a9f1a20895bb5f
shaikareef081 <shaikareef482@gmail.com>
2023-03-04 22:06:53 +0530

    addeed index css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import Header from './components/header/Header';
 import Nav from './components/nav/Nav';
-import About from './components/about/About'
+import About from './components/about/about';
+import Experience from './components/experience/Experience';
+import Services from './components/services/Services';
+import Portfolio from './components/portfolio/Portfolio';
+import Testmonial from './components/testimonials/Testmonial';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 const App = () => {
   return (
     <>
     <Header/>
     <Nav/>
     <About/>
+    <Experience/>
+    <Services/>
+    <Portfolio/>
+    <Testmonial/>
+    <Contact/>
+    <Footer/>
+    
     </>
   )
 }
