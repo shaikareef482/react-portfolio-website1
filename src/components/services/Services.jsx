60a5881df12d12c63e33d7c43fc9050d6a946677
shaikareef081 <shaikareef482@gmail.com>
2023-03-05 19:39:51 +0530

    added experience  css

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,88 @@
 import React from 'react'
-import classes from './services.module.css'
+import classes from './services.module.css';
+import {BiCheck} from "react-icons/bi";
 const Services = () => {
   return (
-    <section id='service'>Services</section>
+    <section id='service'>
+      <h5>What I Offer</h5>
+      <h2>Service</h2>
+      <div className={`container ${classes.services__container}`}>   
+        <article className={classes.service}>
+            <div className={classes.service__head}>
+              <h3>UI/UX Design</h3>
+            </div> 
+            <ul className={classes.service_list}>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            </ul>
+        </article>
+        <article className='service'>
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            
+          </ul>
+        </article>
+        {/* END OF Web Development */}
+        
+        <article className='service'>
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+            <li><BiCheck className='service__list-icon'/>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
