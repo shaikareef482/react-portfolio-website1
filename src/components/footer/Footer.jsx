144d738253fcbd21b0d4538b1d5218da6519de15
shaikareef081 <shaikareef482@gmail.com>
2023-03-06 15:04:38 +0530

    created the footer components

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,36 @@
+/* eslint-disable */
 import React from 'react'
 import classes from './Footer.module.css';
+import {BsLinkedin, BsGithub} from 'react-icons/bs';
+import {SiLeetcode} from 'react-icons/si';
+
 const Footer = () => {
+  const d = new Date();
+  let year = d.getFullYear();
+
   return (
-    <div>Footer</div>
+      <footer>
+        <a href='#' className={classes.footer__logo}>AS</a>
+        <ul className={classes.permalinks}>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+
+        </ul>
+       
+          
+          <div className={classes.footer__socials}>
+            <a href="https://www.linkedin.com" target="_blank"><BsLinkedin/></a>
+            <a  href="https://github.com" target="_blank"><BsGithub /></a>
+            <a href="https://leetcode.com" target="_blank"><SiLeetcode /></a>
+          </div>
+     
+            <div className={classes.footer__copyright}>
+              <small> {year}Areef Shaik</small>
+            </div>
+      </footer>
   )
 }
 
